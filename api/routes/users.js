49d799273c19275b1e0ab45ceec65ad145cddc8e
Nicolas Palavecino <nicopala2013@gmail.com>
2020-03-03 11:54:40 -0300

    first commit

---
+++ api/routes/users.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const checkAuth = require('../middleware/check-auth')
+
+
+const UsersController = require('../controllers/users_ctr')
+
+
+
+router.post('/signup', UsersController.users_signup)
+
+router.post('/login', UsersController.users_login)
+
+router.delete('/:userId', checkAuth, UsersController.users_delete)
+
+module.exports = router
---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const productsRoutes = require('./api/routes/products')
+const ordersRoutes = require('./api/routes/orders')
+const userRoutes = require('./api/routes/users')
+
+mongoose.connect('mongodb+srv://node-shop:'+ process.env.MONGO_ATLAS_PW + '@node-rest-shop-vqa3p.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+mongoose.Promise = global.Promise
+
+
+app.use(morgan('dev'))
+app.use(express.static('uploads'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+
+
+app.use((req, res, next) =>{
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header(
+        'Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Conten-Type, Accept, Authorization'
+    )
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+        return res.status(200).json({})
+    }
+    next()
+})
+
+
+app.use('/products', productsRoutes)
+app.use('/orders', ordersRoutes)
+app.use('/users', userRoutes)
+
+app.use((req, res, next) => {
+    const error = new Error('Not found')
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500)
+    res.json({
+        error:{
+            message : error.message
+        }
+    })
+})
+
+module.exports = app
